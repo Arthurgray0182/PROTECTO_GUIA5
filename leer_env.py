f14c4cbc2ed8e0dcbdcd38ec94592df74cef7d0c
Arthurgray0182 <282095669+Arthurgray0182@users.noreply.github.com>
2026-05-05 21:14:53 -0500

    Add files via upload

---
+++ leer_env.py
@@ -0,0 +1,13 @@
+from dotenv import load_dotenv
+import os
+
+# Carga variables del archivo .env
+load_dotenv()
+
+database = os.getenv("DATABASE_URL")
+debug = os.getenv("DEBUG")
+env = os.getenv("produccion")
+
+print(f"Base de datos: {database}")
+print(f"Modo debug: {debug}")
+print(f"Entorno: {env}")
